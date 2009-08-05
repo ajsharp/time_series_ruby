7d88c375f5caafefdaeadbc4a945132101d80dde
Alex Sharp <ajsharp@gmail.com>
2009-08-04 19:17:12 -0500

    Datapoint is now a nested class inside TimeSeries

--- lib/time_series/datapoint.rb
+++ lib/time_series/datapoint.rb
@@ -1,5 +1,10 @@
 require 'bigdecimal'
-class Datapoint
+
+# Very simple Datapoint model to be used with TimeSeries models
+#
+# Only takes two fields: <tt>:date</tt> and <tt>:value</tt>
+# 
+class TimeSeries::Datapoint
   attr_accessor :date, :value
   
   def initialize(date, value)
