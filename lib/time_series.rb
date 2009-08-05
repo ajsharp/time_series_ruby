d92dc5ab8dfa8a4bb332b3d88ec73280c247d767
Alex Sharp <ajsharp@gmail.com>
2009-08-04 19:16:21 -0500

    requiring the Importer base class, the AbstractImporter class and the CSV importer class

--- lib/time_series.rb
+++ lib/time_series.rb
@@ -3,3 +3,8 @@ require "active_support"
 
 require File.dirname(__FILE__) + "/time_series/time_series"
 require File.dirname(__FILE__) + "/time_series/datapoint"
+
+require File.dirname(__FILE__) + "/time_series/importer"
+require File.dirname(__FILE__) + "/time_series/importers/abstract_importer"
+require File.dirname(__FILE__) + "/time_series/importers/csv"
+
