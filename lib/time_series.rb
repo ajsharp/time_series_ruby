c5420a79679d6839692d258227fbb9bff363c75d
Alex Sharp <ajsharp@gmail.com>
2009-08-08 10:47:12 -0500

    now including TimeSeries::Import class, deleted require of old Importer class

--- lib/time_series.rb
+++ lib/time_series.rb
@@ -4,7 +4,7 @@ require "active_support"
 require File.dirname(__FILE__) + "/time_series/time_series"
 require File.dirname(__FILE__) + "/time_series/datapoint"
 
-require File.dirname(__FILE__) + "/time_series/importer"
+require File.dirname(__FILE__) + "/time_series/import"
 require File.dirname(__FILE__) + "/time_series/importers/abstract_importer"
 require File.dirname(__FILE__) + "/time_series/importers/csv"
 
