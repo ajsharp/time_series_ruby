456d9ae99fd5bc802359ea23cd56856ce87e8dc3
Alex Sharp <ajsharp@gmail.com>
2009-08-08 10:48:34 -0500

    TimeSeries::Importers::CSV now inherits from TimeSeries::Import; refactored superfluous methods into one import! class method

--- lib/time_series/importers/csv.rb
+++ lib/time_series/importers/csv.rb
@@ -3,23 +3,15 @@ require 'fastercsv'
 class TimeSeries
   module Importers
   
-    class CSV < TimeSeries::Importer
+    class CSV < TimeSeries::Import
       include TimeSeries::Importers::AbstractImporter
 
-      def process_file
-        @raw_data = FasterCSV.readlines(@file_name)
+      def self.import!(file_name, context)
+        raw_data = FasterCSV.readlines(file_name)
+        context.headers = raw_data.shift
+        context.datapoints = raw_data.collect { |r| TimeSeries::Datapoint.new(r[0], r[1]) }
       end
 
-      # Returns an array of Datapoint object
-      def extract_datapoints
-        @datapoints = @raw_data.collect { |r| TimeSeries::Datapoint.new(r[0], r[1]) }
-      end
-
-      # This method is expected to return an Array of headers
-      def extract_headers
-        @headers = @raw_data.shift
-      end
-    
     end
 
   end # end of module Importers
